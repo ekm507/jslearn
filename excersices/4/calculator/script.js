c1ead97e0e45ad2f049fcf390ad9178d73181a51
Erfan Kheyrollahi <ekm507@gmail.com>
2026-02-12 16:44:25 +0330

    handle heys

--- excersices/4/calculator/script.js
+++ excersices/4/calculator/script.js
@@ -27,3 +27,37 @@ for (const key of keysLayout) {
   keysContainer.appendChild(button);
   buttons.push(button);
 }
+
+let current = "";
+let operator = null;
+let previous = null;
+
+function handlekey(key, display) {
+  display = document.getElementById("display");
+  if (key in ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]) {
+    if (display.textContent === "0") display.textContent = "";
+    display.textContent += key;
+    current = display.textContent;
+  } else if (key === ".") {
+    if (!display.textContent.includes(".")) {
+      display.textContent += key;
+    } else if (display.textContent[display.textContent.length - 1] === ".") {
+      display.textContent = display.textContent.slice(0, -1);
+    }
+  } else if (key in ["+", "-", "*", "/"]) {
+    operator = key;
+    previous = display.textContent;
+    current = "";
+    display.textContent = "0";
+    calculate();
+  } else if (key === "C") {
+    if (display.textContent === "0") {
+      current = "";
+      operator = null;
+      previous = null;
+    } else {
+      display.textContent = "0";
+    }
+  }
+  console.log(key, display.textContent, operator, previous);
+}
