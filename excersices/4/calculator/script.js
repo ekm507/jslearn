f0bcc55f1a7062b75101c2f16db185c4bb01a8a2
Erfan Kheyrollahi <ekm507@gmail.com>
2026-02-12 17:08:28 +0330

    debug it and now everything worksx

--- excersices/4/calculator/script.js
+++ excersices/4/calculator/script.js
@@ -36,32 +36,32 @@ function handlekey(key, display) {
   display = document.getElementById("display");
   if (["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"].includes(key)) {
     if (current === "0") {
-      display.textContent = "";
       current = "";
     }
     current += key;
+    display.textContent = current;
   } else if (key === ".") {
     if (!current.includes(".")) {
       current += key;
     } else if (current[current.length - 1] === ".") {
       current = current.slice(0, -1);
     }
+    display.textContent = current;
   } else if (["+", "-", "*", "/"].includes(key)) {
-    operator = key;
     calculate();
+    operator = key;
   } else if (key === "C") {
     if (current === "0") {
-      current = "";
       operator = null;
       previous = null;
     } else {
       current = "0";
     }
+    display.textContent = current;
   }
   if (current === "") {
     current = "0";
   }
-  display.textContent = current;
   console.log(key, display.textContent, operator, previous);
 }
 
@@ -80,7 +80,8 @@ function calculate() {
   }
   result = operators[operator](+previous, +current);
   display.textContent = result;
-  current = result;
+  current = "0";
+  previous = result;
 }
 
 buttons.forEach((button) => {
