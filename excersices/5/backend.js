2b775ee1e8080fef4b4013def96203bdfd55ca55
Erfan Kheyrollahi <ekm507@gmail.com>
2026-02-15 00:07:50 +0330

    add cors wildcard

--- excersices/5/backend.js
+++ excersices/5/backend.js
@@ -1,14 +1,16 @@
 const Express = require("express");
 const Database = require("better-sqlite3");
+const cors = require("cors");
 
 const dbfilename = "./db.sqlite3";
 const db = new Database(dbfilename);
 
 const app = new Express();
 app.use(Express.json());
+app.use(cors("*"));
 
 app.get("/", (req, res) => {
-  res.send("Back-end is running");
+  res.send("Back-end is running 😜\n");
 });
 
 app.listen(3000, () => {
